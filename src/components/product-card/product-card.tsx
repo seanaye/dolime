0f48ffd99853c736b40baa42b2437f0c419db282
Sean aye <hello@seanaye.ca>
2021-02-15 21:45:42 -0500

    commit updates to context

--- src/components/product-card/product-card.tsx
+++ src/components/product-card/product-card.tsx
@@ -15,12 +15,17 @@ export const ProductCard: React.FC<{data: ProductCardFragment}> = ({ data }) => 
   function addProduct (event: MouseEvent) {
     // stop click event bubbling to Link
     event.preventDefault()
-    dispatch({ type: 'ADD', id: data.id, price: 1, variant: 'asd' })
+    dispatch({
+      type: 'ADD',
+      id: data.id,
+      price: data.variants.edges[0].node.priceV2.amount,
+      variant: data.variants.edges[0].node.id
+    })
     
   }
 
   return <Link href={`/products/${data.id}`}>
-    <div className="rounded-xl relative bg-white hover:shadow-xl hover:border-purple-400 border-2 border-white hover:border-opacity-75 p-3 cursor-pointer">
+    <div className="rounded-xl relative hover:shadow-xl hover:border-purple-400 border-2 hover:border-opacity-75 p-3 cursor-pointer">
       <div className="absolute top-3 right-3 z-50">
         <RoundedButton onMouseEnter={() => setHover(true)} onMouseLeave={() => setHover(false)} onClick={addProduct}>
           <PlusCircle />
