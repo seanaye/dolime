3af98c643fcf16fc61efb2d6641f053aa9dbb152
Sean aye <hello@seanaye.ca>
2021-02-15 20:57:06 -0500

    display number of cart items on button

--- src/components/cart-button.tsx
+++ src/components/cart-button.tsx
@@ -1,9 +1,16 @@
+import { useContext } from 'react'
 import { RoundedButton } from './rounded-button'
 import Cart from './icons/solid/ShoppingCart'
+import { AppContext } from 'src/contexts/main'
 
-export const CartButton: React.FC = props => {
-  
+export const CartButton: React.FC = () => {
+  const { state } = useContext(AppContext)
+
   return <RoundedButton>
-    <Cart />
+    { !!state.cart.length && 
+      <div className="absolute -top-1 -right-1 rounded-full flex justify-center items-center min-w-min w-5 h-5 bg-red-100">
+        <div>{ state.cart.length }</div>
+      </div>}
+    <Cart className="h-7 w-7" />
   </RoundedButton>
 }
